ce54339e38ed5d2cba32fcb1b24b4e9c4ee9e1a7
Xotic750 <xotic750@gmail.com>
2017-09-26 02:07:31 +0200

    Cache constructor

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @file ES6-compliant shim for ToString.
  * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-tostring|7.1.12 ToString ( argument )}
- * @version 1.4.1
+ * @version 1.4.2
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
@@ -10,6 +10,7 @@
 
 'use strict';
 
+var castString = ''.constructor;
 var isSymbol = require('is-symbol');
 
 /**
@@ -35,5 +36,5 @@ module.exports = function ToString(value) {
     throw new TypeError('Cannot convert a Symbol value to a string');
   }
 
-  return String(value);
+  return castString(value);
 };
