ba60169092402c29a9ec458216d3d665bd086807
Graham Fairweather <xotic750@gmail.com>
2019-07-12 13:56:23 +0200

    :bookmark: v2.0.5

--- dist/to-string-x.js
+++ dist/to-string-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-12T11:51:51.259Z",
+  "date": "2019-07-12T11:56:03.258Z",
   "describe": "",
   "description": "ES6-compliant shim for ToString.",
   "file": "to-string-x.js",
   "hash": "2351d1d9eceb37aa7593",
   "license": "MIT",
-  "version": "2.0.4"
+  "version": "2.0.5"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
